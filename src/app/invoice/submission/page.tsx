3045601e1351ade5b480c1a65e6e91db95d3a105
GustavoBarros360 <gustavobarros2001@gmail.com>
2024-04-18 10:02:26 -0300

    drag-n-drop component

--- src/app/invoice/submission/page.tsx
+++ src/app/invoice/submission/page.tsx
@@ -1,9 +1,13 @@
+import { DragAndDrop } from "@/components/drag-n-drop";
 import { H1 } from "@/components/typography";
 
 export default function InvoiceSubmission() {
   return (
-    <div>
-      <H1>Submeter Invoice</H1>
+    <div className="flex flex-1 h-screen justify-center items-center">
+      <div className="flex flex-col gap-8 align-middle justify-center items-center w-full">
+        <H1>Submeter Invoice</H1>
+        <DragAndDrop />
+      </div>
     </div>
   );
 }
