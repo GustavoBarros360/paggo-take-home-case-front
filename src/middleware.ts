8f578f1c6a9dbb5cc06fc3cf4f2d6f8f413d05d3
GustavoBarros360 <gustavobarros2001@gmail.com>
2024-04-20 19:53:42 -0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,19 @@
 import { NextResponse, NextRequest } from "next/server";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase/firebase.config";
 
 export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  if (path === "/") {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  });
   return NextResponse.next();
 }
 
